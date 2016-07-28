5550d536f6d9db5b057c22184968dc203f15facc
bunker <bunker@achter.be>
2016-07-28 02:28:23 +0200

    changed sendmail to smtp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: '<%= ENV["SMTP_SERVER_ADDR"] %>',
     port: 587,
