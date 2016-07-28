bc8ba1959231ca3f46ea49306b930bf8ee77c523
bunker <bunker@achter.be>
2016-07-28 11:07:29 +0200

    another essay at fixing the authentication error for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     #address: '<%= ENV["SMTP_SERVER_ADDR"] %>',
-    address: 'smtp.sendgrid.net',
+    address: "smtp.sendgrid.net",
     port: 587,
-    user_name: '<%= ENV["SMTP_USERNAME"] %>',
-    password: '<%= ENV["SMTP_PASSWORD"] %>',
+    user_name: "<%= ENV[SMTP_USERNAME] %>",
+    password: "<%= ENV["SMTP_PASSWORD"] %>",
     domain: "fraisdujardin.top",
     authentication: "plain",
     enable_starttls_auto: true,
   }
-  #config.action_mailer.default_url_options = {:host => 'fraisdujardin.top'}
+  config.action_mailer.default_url_options = {:host => 'fraisdujardin.top'}
   config.i18n.locale = :fr
 end
