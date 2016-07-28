2c3db322ed4ca39d2a6852c10d8668e1d29a009d
bunker <bunker@achter.be>
2016-07-28 15:48:42 +0200

    FIXED: ------
    - saving of details

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,12 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :address, :city, :postal_code, :country, :phone, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :email, :address, :city, :postal_code, :country, :phone, :password, :password_confirmation, :current_password)
   end
+
+
 end
